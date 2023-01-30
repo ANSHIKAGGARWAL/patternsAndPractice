66f2e83780c086939eace95b1799a569ec9a2d60
Anshika Aggarwal <121662708+ANSHIKAGGARWAL@users.noreply.github.com>
2023-01-30 15:46:45 +0530

    Create pattern1.c

---
+++ pattern1.c
@@ -0,0 +1,26 @@
+/*Pattern is like
+*
+**
+***
+****    */
+
+#include <stdio.h>
+int main() {
+    int test_cases;
+    printf("Enter the number of test cases \t");
+    scanf("%d",&test_cases);
+    while(test_cases--) {
+        int number,i,j;
+        printf("Enter the number of stars to build a pyramid \t");
+        scanf("%d",&number);
+        for(i=1;i<=number;i++)
+        {
+            printf("\n");
+            for(j=1;j<=i;j++)
+            {
+                printf("*");
+            }
+        }
+        printf("\n");
+    }
+    return 0; }
